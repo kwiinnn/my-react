9d3836f5f2e90ef311111a26f8e14e49ba820527
kwiinnn <solomon23charles@gmail.com>
2026-02-16 00:05:14 +0800

    Transferred checkout page to react

--- ChaStore/src/App.jsx
+++ ChaStore/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<HomePage />}>
-      </Route>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
       
     </>
