7e00efb0e5807dfca3a6c03221360021acb4463c
kwiinnn <solomon23charles@gmail.com>
2026-02-19 21:07:20 +0800

    Created add to cart feature

--- ChaStore/src/pages/home/HomePage.jsx
+++ ChaStore/src/pages/home/HomePage.jsx
@@ -4,28 +4,24 @@ import { Header } from "../../components/Header";
 import { ProductsGrid } from "./ProductsGrid";
 import "./HomePage.css";
 
-export function HomePage( {cart} ) {
-  const [ products, setProducts ] = useState([]);
-  
+export function HomePage({ cart, loadCart }) {
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    }
+    };
     getHomeData();
   }, []);
-    
-
-
 
   return (
     <>
       <title>ChaStore</title>
 
-      <Header cart={cart}/>
+      <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
