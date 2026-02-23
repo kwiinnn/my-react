70ee5345d435ebd13ddeebe6bbea955bb0cf9af7
kwiinnn <solomon23charles@gmail.com>
2026-02-23 17:49:50 +0800

    Transferred chastore to TypeScript

---
+++ chastore-ts/src/pages/orders/OrdersPage.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Header } from "../../components/Header";
+import "./OrdersPage.css";
+import { OrdersGrid } from "./OrdersGrid";
+
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+
+    const fetchOrdersData = async () => {
+      const response = await axios.get("/api/orders?expand=products")
+      setOrders(response.data);
+    }
+    fetchOrdersData();
+  }, []);
+
+  return (
+    <>
+      <title>Orders</title>
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+        <OrdersGrid orders={orders} />
+      </div>
+    </>
+  );
+}
